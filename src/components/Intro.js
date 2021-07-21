6df382c2af6cd03f5cd18dd48777cad4b70e929a
Zoe Jones <P2423891@my365.dmu.ac.uk>
2021-07-21 18:07:42 +0100

    doesnt work!

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,14 @@
-import { useState, useEffect } from 'react';
-import { ChainId, DAppProvider, useContractCall, useEthers,ERC20Interface } from '@usedapp/core'
-import { formatEther } from '@ethersproject/units'
+import { useState, useEffect, setDisabled } from 'react';
+import { ChainId, DAppProvider, useContractCall, useEthers, ERC20Interface, useSendTransaction, useContractFunction } from '@usedapp/core'
+import { formatEther, parseEther } from '@ethersproject/units'
+import { utils } from 'ethers'
+import { Contract } from '@ethersproject/contracts'
 
 const Intro = () => {
+  const interface_ = new utils.Interface(ERC20Interface)
+  const ContractAddress = '0x9f683b4910c31effc564e39a77c9159287d0770f'
+  const contract = new Contract(ContractAddress, interface_)
+
   const [data, setData] = useState({})
 
   useEffect(async () => {
@@ -30,6 +36,12 @@ const Intro = () => {
 
   const { activateBrowserWallet, account } = useEthers()
   const etherBalance = useTokenBalance('0x9f683b4910c31effc564e39a77c9159287d0770f',account)
+  const { sendTransaction, state } = useSendTransaction()
+
+  const { state_, send } = useContractFunction(contract, 'transfer', { transactionName: 'transfer' })
+  const transferToken = (etherAmount: string) => {
+    send({ address:'0xe065cC27EE71e73F0d19bf190f3B81bB9Ef4E097',value: parseEther(etherAmount) })
+  }
 
   return (
     <div>
@@ -42,6 +54,9 @@ const Intro = () => {
         <h3>Ethers test</h3>
           <div>
             <button className='button' onClick={() => activateBrowserWallet()}>Connect</button>
+          </div>
+          <div>
+            <button className='button' onClick={() => sendTransaction({ to: '0x0000000000000000000000000000000000000000', value: parseEther('0.009') })}>Send ether</button>
           </div>
           {account && <p>Account: {account}</p>}
           {etherBalance && <p>Balance: {formatEther(etherBalance)} moagCoins</p>}
