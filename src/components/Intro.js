8c7b5df13c7cddc72b928746dfa88c0f3adc7bab
Zoe Jones <P2423891@my365.dmu.ac.uk>
2021-07-22 19:25:16 +0100

    yeah it works haha

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,12 @@
 import { useState, useEffect, setDisabled } from 'react';
-import { ChainId, DAppProvider, useContractCall, useEthers, ERC20Interface, useSendTransaction, useContractFunction } from '@usedapp/core'
+import { ChainId, DAppProvider, useContractCall, useEthers, useSendTransaction, useContractFunction } from '@usedapp/core'
 import { formatEther, parseEther } from '@ethersproject/units'
+import ERC20Interface from '../TestToken.json';
 import { utils } from 'ethers'
 import { Contract } from '@ethersproject/contracts'
 
 const Intro = () => {
-  const interface_ = new utils.Interface(ERC20Interface)
+  const interface_ = new utils.Interface(ERC20Interface.abi)
   const ContractAddress = '0x9f683b4910c31effc564e39a77c9159287d0770f'
   const contract = new Contract(ContractAddress, interface_)
 
@@ -18,15 +19,14 @@ const Intro = () => {
   }, []);
 
   function useTokenBalance(
-    tokenAddress: string | Falsy,
     address: string | Falsy
   ) {
     const [allowance] =
       useContractCall(
           address &&
-          tokenAddress && {
-            abi: ERC20Interface,
-            address: tokenAddress,
+            {
+            abi: interface_,
+            address: ContractAddress,
             method: 'balanceOf',
             args: [address],
           }
@@ -35,12 +35,12 @@ const Intro = () => {
   }
 
   const { activateBrowserWallet, account } = useEthers()
-  const etherBalance = useTokenBalance('0x9f683b4910c31effc564e39a77c9159287d0770f',account)
+  const etherBalance = useTokenBalance(account)
   const { sendTransaction, state } = useSendTransaction()
 
-  const { state_, send } = useContractFunction(contract, 'transfer', { transactionName: 'transfer' })
-  const transferToken = (etherAmount: string) => {
-    send({ address:'0xe065cC27EE71e73F0d19bf190f3B81bB9Ef4E097',value: parseEther(etherAmount) })
+  const { state_, send } = useContractFunction(contract, 'transfer')
+  const transferToken = (receiver: string, numTokens: string) => {
+    send(receiver, parseEther(numTokens))
   }
 
   return (
@@ -57,6 +57,9 @@ const Intro = () => {
           </div>
           <div>
             <button className='button' onClick={() => sendTransaction({ to: '0x0000000000000000000000000000000000000000', value: parseEther('0.009') })}>Send ether</button>
+          </div>
+          <div>
+            <button className='button' onClick={() => transferToken('0xe065cC27EE71e73F0d19bf190f3B81bB9Ef4E097','5')}>Send shit</button>
           </div>
           {account && <p>Account: {account}</p>}
           {etherBalance && <p>Balance: {formatEther(etherBalance)} moagCoins</p>}
